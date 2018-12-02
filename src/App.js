4f2c78840c2c85b3b68c2720082bdcdc30fd658e
alison <alison.haire@accenture.com>
2018-12-02 17:17:09 +1100

    dnd chessboard

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { DragDropContextProvider } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
+import WorkflowPage from "./Components/Workflow/WorkflowPage";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <DragDropContextProvider backend={HTML5Backend}>
+        <div className="App">
+          <WorkflowPage />
+        </div>
+      </DragDropContextProvider>
     );
   }
 }
 
 export default App;
+
+// import React, { Component } from 'react';
+// import WorkflowSelector from "./components/WorkflowSelector";
+// import VXComponent from "./components/VXComponent";
+// import MyDiagram from "./Components/MyDiagram";
+//
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <div>lets try styled components</div>
+//         <WorkflowSelector />
+//         <div style={{ width: "100%", height: "100%", minHeight: "500px", backgroundColor: "lightblue" }}>
+//           <MyDiagram />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+//
+// export default App;
+// <header className="App-header">
+//   <img src={logo} className="App-logo" alt="logo" />
+//   <h1 className="App-title">Make a tree</h1>
+// </header>
+// <div style={{ minHeight: "500px", minWidth: "500px" }}>
+//   <VXDendo width="500px" height="500px" />
+// </div>
